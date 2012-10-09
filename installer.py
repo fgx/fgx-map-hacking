f4f2e7eed15d912a2c0f4dd3847428d66e53113e
Peter Morgan <pete@daffodil.uk.com>
2012-10-09 19:40:30 +0100

    installer idea

--- installer.py
+++ installer.py
@@ -8,6 +8,37 @@ from optparse import OptionParser
 import fgx.app_global as G
 from fgx.config import config
 
+## Handle Command Args
+usage = "usage: %prog [options] command\n"
+usage += " command = check"
+parser = OptionParser(usage=usage)
+parser.add_option(	"-j", 
+					action="store_true", dest="js", default=False, 
+					help="Write javascript configuration to `/etc/config.js`"
+					)   
+parser.add_option(	"-n", 
+					action="store_true", dest="nginx", default=False, 
+					help="Write nginx config to `/etc/nginx.conf`"
+					) 
+parser.add_option(	"-s", 
+					action="store_true", dest="ln", default=False, 
+					help="Write symlinks"
+					)
+parser.add_option(	"-t", 
+					action="store_true", dest="tilecache", default=False, 
+					help="Write tilecache"
+					) 
+parser.add_option(	"-v", nargs=1,
+					action="store", type="int", dest="v", default=1,
+					help="Prints more output 0-4 (0=none, 4=loads)"
+                  )
+parser.add_option(	"--make-local", 
+					action="store_true", dest="local", default=False, 
+					help="create local config file to `/local_config.yaml`"
+					) 
+(opts, args) = parser.parse_args()
+
+print opts, args
 
 print G.APT_PACKAGES
 
