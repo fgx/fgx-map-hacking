9e27cca82f033a40d91e247997c9631cfe21692e
Peter Morgan <pete@daffodil.uk.com>
2012-10-11 17:35:46 +0100

    nuked inadventently added pylons

--- fgx-map-consolidation/fgxmap/fgxmap/tests/functional/test_appinfo.py
+++
@@ -1,7 +0,0 @@
-from fgxmap.tests import *
-
-class TestAppinfoController(TestController):
-
-    def test_index(self):
-        response = self.app.get(url(controller='appinfo', action='index'))
-        # Test response...
