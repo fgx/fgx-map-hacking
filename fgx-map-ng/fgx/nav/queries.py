6507d079cc1ccd6a9c4bc77813af06cc094e5b7a
Peter Morgan <pete@daffodil.uk.com>
2012-10-21 22:54:32 +0100

    queries for fix

--- fgx-map-ng/fgx/nav/queries.py
+++ fgx-map-ng/fgx/nav/queries.py
@@ -1,11 +1,19 @@
 
+from django.db import connection, transaction
+
+cursor = connection.cursor()
+    
 
 def fix(search=None, ident=None):
 	
 	## list columns seperated by spaces
 	cols = "fix lat lon".split()
 	
-	sql = "select fix, la"
+	sql = "select fix_pk, fix from fix where fix like '%%%s%%';" % search
 	
 	
+	cursor.execute(sql)
 	
+	rows = cursor.fetchall()
+	
+	return rows
