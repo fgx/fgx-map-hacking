81dd235ec7e6ac57fb15d39794ad46528ec6cca5
Peter Morgan <pete@daffodil.uk.com>
2012-10-23 20:16:02 +0100

    ext now loads

--- fgx-map-ng/flask_app/fgx/modules/xmap/views.py
+++ fgx-map-ng/flask_app/fgx/modules/xmap/views.py
@@ -5,11 +5,12 @@ from xmap import module
 class Context(object):
 	pass
 
+	
 @module.route('/')
 def index():
 	c = Context()
 	c.remote_addr = request.environ['REMOTE_ADDR']
-    
+	c.static_url = "http://static.fgx.ch"
 	return render_template('xmap/index.html', c=c)
 
-    
+	
