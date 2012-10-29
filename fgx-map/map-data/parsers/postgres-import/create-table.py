b4292099e8e849ca6c7c1b37f3b9836da1ba8174
Yves Sablonier <gral@who.net>
2012-10-29 11:13:51 +0100

    Create table with yaml

--- fgx-map/map-data/parsers/postgres-import/create-table.py
+++ fgx-map/map-data/parsers/postgres-import/create-table.py
@@ -11,22 +11,36 @@ conf = open('database.yaml')
 confMap = yaml.load(conf)
 conf.close()
 
+fields = open('airport.yaml')
+airportMap = yaml.load(fields)
+fields.close()
+
+sqlstring = "CREATE TABLE airport ("
+
+for i in airportMap.keys():
+	if airportMap[i]['size'] == None:
+		sqlstring += airportMap[i]['field'] + " " + airportMap[i]['type'] + ","# \\\n"
+	else:
+		sqlstring += airportMap[i]['field'] + " " + airportMap[i]['type'] + "(" + str(airportMap[i]['size']) + ")" + ","# \\\n"
+	
+sqlstring = sqlstring + ");"
+exe = sqlstring.replace(",);",");")
+
+print sqlstring
+
 connectstring = "dbname=" + confMap['database'] + " user=" + confMap['user'] + " password=" + confMap['password']
 
 conn = psycopg2.connect(connectstring)
 cur = conn.cursor()
 			
 cur.execute("DROP TABLE IF EXISTS airport;")
-cur.execute("CREATE TABLE airport (apt_id serial PRIMARY KEY, \
-			apt_gps_code varchar, \
-			apt_name_ascii varchar, \
-			apt_elev_ft varchar, \
-			apt_center geometry(Point,3857));")
+
+cur.execute(exe)
 
 
 conn.commit()
 cur.close()
 conn.close()
 
-print "--- CREATED TABLE AIRPORT ---"
+print "--- CREATED TABLE: AIRPORT ---"
 
