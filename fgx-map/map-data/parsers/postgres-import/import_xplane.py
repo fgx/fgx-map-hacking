cdb44343c1f06edce85bff1d4ecbf660596decc1
Yves Sablonier <gral@who.net>
2012-10-30 23:17:24 +0100

    Being sure with files

--- fgx-map/map-data/parsers/postgres-import/import_xplane.py
+++ fgx-map/map-data/parsers/postgres-import/import_xplane.py
@@ -325,23 +325,12 @@ def readxplane():
 			
 			wwy_approach_lighting = "0"
 			
-			#print wwy_linecode 
-			#print wwy_width 
-			#print wwy_buoys
-			#print wwy_id
-			#print wwy_lat
-			#print wwy_lon
-			
-			#print wwy_id_end
-			#print wwy_lat_end
-			#print wwy_lon_end
-			
 			# Now some additional data, not in apt.dat
 			
 			wwy_length = Geodesic.WGS84.Inverse(float(wwy_lat), float(wwy_lon), float(wwy_lat_end), float(wwy_lon_end))
 			wwy_length_meters = str(wwy_length.get("s12"))
 			
-			print "Meters: "+wwy_length_meters
+			#print "Meters: "+wwy_length_meters
 			
 			wwy_length_feet = wwy_length.get("s12")*3.048
 			
@@ -370,6 +359,69 @@ def readxplane():
 			# Collecting runway points
 			points = str(A_lon) + " " + str(A_lat) + "," + str(B_lon) + " " + str(B_lat) + "," + str(C_lon) + " " + str(C_lat) + "," + str(D_lon) + " " + str(D_lat) + ","
 			collecting(points, wwy_length_meters, wwy_approach_lighting)
+			
+		# HELIPADS, we need it for heliport calculation, i.e. centerpoint
+		if line.startswith("102 "):
+		
+			pad_linecode = line[0:3]
+			pad_id = str(line[4:6])
+			pad_lat_read = line[7:19]
+			pad_lon_read = line[21:33]
+			pad_heading_tr = line[34:41]
+			pad_length_m = line[42:49]
+			pad_width_m = line[50:57]
+			pad_surface = line[58:61]
+			pad_marking = line[61:63]
+			pad_shoulder = line[65:67]
+			pad_smoothness = line[68:72]
+			pad_edgelighting = line[73]
+			
+			pad_approach_lighting = "0"
+			
+			# Now some additional data, not in apt.dat
+			
+			# We get the centerpoint of the pad, but we want the center point at shape start, 
+			# to get it calculated like runways and to prevent me from writing all separated for pads
+			pad_direct = Geodesic.WGS84.Direct(float(pad_lat_read),float(pad_lon_read),float(pad_heading_tr-180.0),(float(pad_length_m))/2)
+			pad_lat = pad_direct.get("lat2")
+			pad_lon = pad_direct.get("lon2")
+			
+			pad_direct_end = Geodesic.WGS84.Direct(float(pad_lat_read),float(pad_lon_read),float(pad_heading_tr),(float(pad_length_m)))
+			pad_lat_end = pad_direct_end.get("lat2")
+			pad_lon_end = pad_direct_end.get("lon2")
+			
+			pad_length = Geodesic.WGS84.Inverse(float(pad_lat), float(pad_lon), float(pad_lat_end), float(pad_lon_end))
+			pad_length_meters = str(pad_length.get("s12"))
+			
+			print "Meters: "+pad_length_m
+			
+			pad_length_ft = pad_length_m*3.048
+			
+			pad_heading = pad_length.get("azi2")
+			
+			pad_length_end = Geodesic.WGS84.Inverse(float(pad_lat_end), float(pad_lon_end), float(pad_lat), float(pad_lon))
+			pad_heading_end = str(360.0 + pad_length_end.get("azi2"))
+
+			# Calculating runway points
+			pad_direct_A = Geodesic.WGS84.Direct(float(pad_lat),float(pad_lon),float(pad_heading-90.0),(float(pad_width))/2)
+			A_lat = pad_direct_A.get("lat2")
+			A_lon = pad_direct_A.get("lon2")
+			
+			pad_direct_B = Geodesic.WGS84.Direct(float(pad_lat),float(pad_lon),float(pad_heading+90.0),(float(pad_width))/2)
+			B_lat = pad_direct_B.get("lat2")
+			B_lon = pad_direct_B.get("lon2")
+			
+			pad_direct_C = Geodesic.WGS84.Direct(float(pad_lat_end),float(pad_lon_end),-360.0 + float(pad_heading_end)-90.0,(float(pad_width))/2)
+			C_lat = pad_direct_C.get("lat2")
+			C_lon = pad_direct_C.get("lon2")
+			
+			pad_direct_D = Geodesic.WGS84.Direct(float(pad_lat_end),float(pad_lon_end),-360.0 + float(pad_heading_end)+90.0,(float(pad_width))/2)
+			D_lat = pad_direct_D.get("lat2")
+			D_lon = pad_direct_D.get("lon2")
+			
+			# Collecting runway points
+			points = str(A_lon) + " " + str(A_lat) + "," + str(B_lon) + " " + str(B_lat) + "," + str(C_lon) + " " + str(C_lat) + "," + str(D_lon) + " " + str(D_lat) + ","
+			collecting(points, pad_length_meters, pad_approach_lighting)
 			
 			
 		# One green and two white flashes means military airport - no civil aircraft allowed.
