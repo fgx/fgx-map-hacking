3a52f062a723433069a63b90c2f67eec272b8da0
Peter Morgan <pete@daffodil.uk.com>
2012-11-15 21:15:46 +0000

    clean up globals

--- fgx-map-ng/fgx/fgx/lib/app_globals.py
+++ fgx-map-ng/fgx/fgx/lib/app_globals.py
@@ -4,15 +4,23 @@ from beaker.cache import CacheManager
 from beaker.util import parse_cache_config_options
 
 class Globals(object):
-    """Globals acts as a container for objects available throughout the
-    life of the application
+	"""Globals acts as a container for objects available throughout the
+	life of the application
 
-    """
+	"""
 
-    def __init__(self, config):
-        """One instance of Globals is created during application
-        initialization and is available during requests via the
-        'app_globals' variable
+	def __init__(self, config):
+		"""One instance of Globals is created during application
+		initialization and is available during requests via the
+		'app_globals' variable
 
-        """
-        self.cache = CacheManager(**parse_cache_config_options(config))
+		"""
+		self.cache = CacheManager(**parse_cache_config_options(config))
+
+		
+		static_url = "http://static.fgx.ch"
+
+
+
+		#FGX_MP_SERVER = "mpserver14.flightgear.org"
+		fgx_ = "217.150.241.103"
