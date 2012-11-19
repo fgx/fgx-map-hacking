8c0e552219ac43116b31bead8cee6187e6395567
Peter Morgan <pete@daffodil.uk.com>
2012-11-19 22:32:44 +0000

    View Port refactor

--- fgx-map-ng/fgxmap_pylons/fgx/public/js/fgx/MainViewport.js
+++ fgx-map-ng/fgxmap_pylons/fgx/public/js/fgx/MainViewport.js
@@ -1,131 +1,93 @@
 
 Ext.namespace("FGx");
 
-FGx.MainViewport = function(){
+Ext.namespace("FGx");
 
-var self = this;	
+FGx.MainViewport = Ext.extend(Ext.Viewport, {
 
-var zooms = [1, 2, 3, 4, 5, 7, 9, 10, 20, 50, 73, 100, 150, 250];
-//this.centerpoint = new OpenLayers.LonLat(939262.20344,5938898.34882);	
 	
-this.refresh_rate = 0;
-this.runner = new Ext.util.TaskRunner();
-
 //===========================================================
 //== FlightsStore
-this.flightsStore = new Ext.data.JsonStore({
-	idProperty: 'callsign',
-	fields: [ 	{name: 'flag', type: 'int'},
-				{name: 'check', type: 'int'},
-				{name: "callsign", type: 'string'},
-				{name: "server", type: 'string'},
-				{name: "model", type: 'string'},
-				{name: "lat", type: 'float'},
-				{name: "lon", type: 'float'},
-				{name: "alt_ft", type: 'int'},
-				{name: "spd_kts", type: 'int'},
-				//{name: "alt_trend", type: 'string'},
-				{name: "heading", type: 'string'}
-	],
-	url: '/ajax/mp/flights/crossfeed',
-	root: 'flights',
-	remoteSort: false,
-	sortInfo: {
-		field: "callsign", 
-		direction: 'ASC'
-	},
-	autoLoad: false,
-});
 
-this.update_flights = function(){
-	self.flightsStore.load();
-}
+refresh_rate: 0,
+runner: new Ext.util.TaskRunner(),
 
-
-
-
-//=================================================================================
-// Other Widgets - Note the Map is passed in constructor as ref
-//============================================================
-//this.flightsWidget = new FGx.FlightsWidget({});
-this.flightsGrid = new FGx.FlightsGrid({
-	flightsStore: this.flightsStore,
-	refresh_rate: this.refresh_rate,
-	title: "Flights", 
-	closable: true,
-	xHidden: false
-});
-
-
-/*
-this.settingsWidget = new FGx.SettingsWidget({
-	runner: this.runner, refresh_rate: this.refresh_rate
-});
-this.settingsWidget.on("SET_REFRESH", function(rate){
-	//console.log("SET_REFRESH", rate);
-	this.runner.stopAll(); // stop if already running
-	this.refresh_rate = rate;
-	if(this.refresh_rate === 0){
-		//this.runner.stop()
-	}else{
-		this.runner.start( { run: this.update_flights, interval: this.refresh_rate * 1000 });
+get_flights_store: function(){
+	if(!this.xFlightsStore){
+		this.xFlightsStore = new Ext.data.JsonStore({
+			idProperty: 'callsign',
+			fields: [ 	{name: 'flag', type: 'int'},
+						{name: 'check', type: 'int'},
+						{name: "callsign", type: 'string'},
+						{name: "server", type: 'string'},
+						{name: "model", type: 'string'},
+						{name: "lat", type: 'float'},
+						{name: "lon", type: 'float'},
+						{name: "alt_ft", type: 'int'},
+						{name: "spd_kts", type: 'int'},
+						//{name: "alt_trend", type: 'string'},
+						{name: "heading", type: 'string'}
+			],
+			url: '/ajax/mp/flights/crossfeed',
+			root: 'flights',
+			remoteSort: false,
+			sortInfo: {
+				field: "callsign", 
+				direction: 'ASC'
+			},
+			autoLoad: false,
+		});
 	}
-}, this);
-*/
+	return this.xFlightsStore;
+},
 
-//this.mpStatusGrid = new FGx.MpStatusGrid({flightsStore: this.flightsStore, title: "Server Status"});
+update_flights: function(){
+	this.get_flights_store.load();
+},
+
+
+flightsGrid: 0,
+
+load_flights_grid: function(){
+	this.flightsGrid = new FGx.FlightsGrid({
+		flightsStore: this.flightsStore,
+		refresh_rate: this.refresh_rate,
+		title: "Flights", 
+		closable: true,
+		xHidden: false
+	});
+	
+},
+
+
 
 //=================================================================================
 // Map Panels
 //=================================================================================
 
-this.on_open_map = function(title, lat, lon, zoom, closable){
+on_open_map:  function(title, lat, lon, zoom, closable){
 	var newMap = new FGx.MapPanel({
 		title: title, closable: true, 
 		flightsStore: self.flightsStore,
 		lat: lat, lon: lon, zoom: zoom
 	});
-	this.tabPanel.add(newMap);
-	this.tabPanel.setActiveTab(newMap);
+	this.get_tab_panel().add(newMap);
+	this.get_tab_panel().setActiveTab(newMap);
 	
-};
+},
 
-this.on_goto = function(butt){
+on_goto: function(butt){
 	//var lonLat = new OpenLayers.LonLat(
 	//		).transform(this.get_display_projection(),  this.get_map().getProjectionObject() );
 	//this.fireEvent("OPEN_MAP", butt.text, true, lonLat, butt.zoom);
 	this.on_open_map( butt.text, true, butt.lon, butt.lat, butt.zoom);
-}
+},
 
 
-
-//this.mapPanels.mainMap.init();
-//this.mapPanels.mainMap.on("OPEN_MAP", this.on_open_map, this);
-	
-
-
-//this.mapPanels.base2 = new FGx.MapPanel({title: "Map 2", closable: true, flightsStore: this.flightsStore});
-//this.mapPanels.base2.init();
-
-this.tabPanel = new Ext.TabPanel({
-	region: "center",
-	tabPosition: "top",
-	frame: false, plain: true, border: false, bodyBorder: false,
-	activeItem: 0,
-	items: [
-		
-		//this.mapPanels.mainMap,
-		//this.mapPanels.base2,
-		
-		this.flightsGrid,
-		//this.mpStatusGrid,
-		//this.settingsWidget
-	]
-	
-});
 
 //this.mapPanels = {};
-this.add_map = function(title, closable, lonLat, zoom, idx){
+/*
+ssadd_map: function(title, lat lon, zoom, closable){
 	var mapPanel = new FGx.MapPanel({
 		title: title, closable: closable, flightsStore: self.flightsStore,
 		lonLat: lonLat, zoom: zoom
@@ -137,132 +99,136 @@ this.add_map = function(title, closable, lonLat, zoom, idx){
 		self.tabPanel.add(mapPanel);
 	}
 	self.tabPanel.setActiveTab(mapPanel);
-	mapPanel.on("OPEN_MAP", self.add_map, self);
-}
-
-this.add_map("Main Map", false, false, false, 0);
-
-
-
-
-this.viewport = new Ext.Viewport({
-	layout: "border",
-	frame: false,
-	plain: true,
-	border: false,
+	//mapPanel.on("OPEN_MAP", self.add_map, self);
+},
+*/
+//=======================================
+//== Tab Panel
+get_tab_panel: function(){
 	
-	items: [
+	if(!this.xTabPanel){
+		this.xTabPanel = new Ext.TabPanel({
+			region: "center",
+			tabPosition: "top",
+			frame: false, plain: true, border: false, bodyBorder: false,
+			activeItem: 0
+		});
+	}
+	return this.xTabPanel;
+},
 
-		//this.mapPanel,
-		this.tabPanel,
+
+
+
+//=======================================
+//== Contructor
+constructor: function(config) {
+	
+	config = Ext.apply({
+		layout: "border",
+		frame: false,
+		plain: true,
+		border: false,
+		items: [
 		
-		{xtype: "panel",
-			region: "north",
-			frame: false, plain: true, border: false, hideBorders: true,
-			margins: {top:0, right:0, bottom:5, left:0},
-			hideHeader: true,
-			tbar: [
-				{xtype: 'tbspacer', width: 5},
-				"-",
-				{text: "New Map", iconCls: "icoMapAdd", 				
-					menu: [
-						{text: "World", handler: this.on_goto, scope: this,
-							zoom: 5, lat: 47.467, lon: 8.5597,
-						},
-						"-",
-						{text: "Africa", disabled: true },
-						{text: "Austrailia" , disabled: true},
-						{text: "Europe" , disabled: true},
-						{text: "Far East" , disabled: true},
+			// TabBar in Center
+			this.get_tab_panel(),
+		
+			//= Top Toolbar = North
+			{xtype: "panel",
+				region: "north",
+				frame: false, plain: true, border: false, hideBorders: true,
+				margins: {top:0, right:0, bottom:5, left:0},
+				hideHeader: true,
+				tbar: [
+					{xtype: 'tbspacer', width: 5},
+					"-",
+					{text: "New Map", iconCls: "icoMapAdd", 				
+						menu: [
+							{text: "World", handler: this.on_goto, scope: this,
+								zoom: 5, lat: 47.467, lon: 8.5597,
+							},
+							"-",
+							{text: "Africa", disabled: true },
+							{text: "Austrailia" , disabled: true},
+							{text: "Europe" , disabled: true},
+							{text: "Far East" , disabled: true},
+							
+							{text: "USA" , disabled: true},
+							"-",
+							{text: "Amsterdam", aptIdent: "EHAM", lat: 52.306, lon:4.7787, zoom: 10,
+								handler: this.on_goto, scope: this},
+							{text: "London", aptIdent: "EGLL",  lat: 51.484, lon: -0.1510, zoom: 10,
+								handler: this.on_goto, scope: this},
+							{text: "Paris", aptIdent: "LFPG", lat: 48.994, lon: 2.650, zoom: 10,
+								handler: this.on_goto, scope: this},
+							{text: "San Fransisco", aptIdent: "KSFO", lat: 37.621302, lon: -122.371216, zoom: 10,
+								handler: this.on_goto, scope: this},
+							{text: "Zurich", aptIdent: "LSZH", lat: 47.467, lon: 8.5597, zoom: 10,
+								handler: this.on_goto, scope: this},
+						]
+					},
+					"-",
+					{xtype: 'tbspacer', width: 50},
+					"-",
+					{text: "Flights", iconCls: "icoFlights", enableToggle: true, pressed: true},
+					
+					"-",
+					{text: "Server Status", iconCls: "icoMpServers"},
+					"-",
+					//{text: "Settings", iconCls: "icoSettings"},
+					//"-",
+					{xtype: 'tbspacer', width: 50},
+					"-",
+					
+					{text: "Now", iconCls: "icoRefresh",  handler: this.on_refresh_now, scope: this},
+					{text: "Off", iconCls: "icoOn", pressed: true, enableToggle: true, scope: this,
+						toggleGroup: "ref_rate", ref_rate: 0, toggleHandler: this.on_refresh_toggled},
+					{text: "2", iconCls: "icoOff", enableToggle: true,   scope: this, width: this.tbw,
+						toggleGroup: "ref_rate", ref_rate: 2, toggleHandler: this.on_refresh_toggled},
+					{text: "3", iconCls: "icoOff", enableToggle: true,  scope: this,  width: this.tbw,
+						toggleGroup: "ref_rate", ref_rate: 3, toggleHandler: this.on_refresh_toggled},
+					{text: "4", iconCls: "icoOff", enableToggle: true,  scope: this,  width: this.tbw,
+						toggleGroup: "ref_rate", ref_rate: 4, toggleHandler: this.on_refresh_toggled},
+					{text: "5", iconCls: "icoOff", enableToggle: true,  scope: this,  width: this.tbw,
+						toggleGroup: "ref_rate", ref_rate: 5, toggleHandler: this.on_refresh_toggled},
+					{text: "10", iconCls: "icoOff", enableToggle: true,   scope: this, width: this.tbw,
+						toggleGroup: "ref_rate", ref_rate: 6, toggleHandler: this.on_refresh_toggled},
 						
-						{text: "USA" , disabled: true},
-						"-",
-						{text: "Amsterdam", aptIdent: "EHAM", lat: 52.306, lon:4.7787, zoom: 10,
-							handler: this.on_goto, scope: this},
-						{text: "London", aptIdent: "EGLL",  lat: 51.484, lon: -0.1510, zoom: 10,
-							handler: this.on_goto, scope: this},
-						{text: "Paris", aptIdent: "LFPG", lat: 48.994, lon: 2.650, zoom: 10,
-							handler: this.on_goto, scope: this},
-						{text: "San Fransisco", aptIdent: "KSFO", lat: 37.621302, lon: -122.371216, zoom: 10,
-							handler: this.on_goto, scope: this},
-						{text: "Zurich", aptIdent: "LSZH", lat: 47.467, lon: 8.5597, zoom: 10,
-							handler: this.on_goto, scope: this},
-					]
-								
-				},
-				"-",
-				{xtype: 'tbspacer', width: 50},
-				"-",
-				{text: "Flights", iconCls: "icoFlights", enableToggle: true, pressed: true},
-				
-				"-",
-				{text: "Server Status", iconCls: "icoMpServers"},
-				"-",
-				//{text: "Settings", iconCls: "icoSettings"},
-				//"-",
-				{xtype: 'tbspacer', width: 50},
-				"-",
-				
-				{text: "Now", iconCls: "icoRefresh",  handler: this.on_refresh_now, scope: this},
-				{text: "Off", iconCls: "icoOn", pressed: true, enableToggle: true, scope: this,
-					toggleGroup: "ref_rate", ref_rate: 0, toggleHandler: this.on_refresh_toggled},
-				{text: "2", iconCls: "icoOff", enableToggle: true,   scope: this, width: this.tbw,
-					toggleGroup: "ref_rate", ref_rate: 2, toggleHandler: this.on_refresh_toggled},
-				{text: "3", iconCls: "icoOff", enableToggle: true,  scope: this,  width: this.tbw,
-					toggleGroup: "ref_rate", ref_rate: 3, toggleHandler: this.on_refresh_toggled},
-				{text: "4", iconCls: "icoOff", enableToggle: true,  scope: this,  width: this.tbw,
-					toggleGroup: "ref_rate", ref_rate: 4, toggleHandler: this.on_refresh_toggled},
-				{text: "5", iconCls: "icoOff", enableToggle: true,  scope: this,  width: this.tbw,
-					toggleGroup: "ref_rate", ref_rate: 5, toggleHandler: this.on_refresh_toggled},
-				{text: "10", iconCls: "icoOff", enableToggle: true,   scope: this, width: this.tbw,
-					toggleGroup: "ref_rate", ref_rate: 6, toggleHandler: this.on_refresh_toggled},
-					
-				"->",	
-				"-",	
-				{text: "Login", iconCls: "icoLogin"},
-				"-",
-				{text: "FGx", iconCls: "icoFgx", 
-					menu: [
-						{text: "Database Browser" }
-					]
-				},
-				{xtype: 'tbspacer', width: 10}
-					
-			]
-		}
-		
-		/* {region: 'east', width: 400, 
-			title: "FGx Map - Next Gen",
-			xtype: 'tabpanel',
-			frame: false,
-			plain: true,
-			border: 0,
-			collapsible: true,
-			activeItem: 0,
-			items: [
-				//this.mapLayersTree.tree,
-				this.flightsGrid,
-				//this.flightsWidget.grid,
-				this.navWidget.grid
-				
-			]
-        
-		},
-		*/
-	]
-});
+					"->",	
+					"-",	
+					{text: "Login", iconCls: "icoLogin"},
+					"-",
+					{text: "FGx", iconCls: "icoFgx", 
+						menu: [
+							{text: "Database Browser" }
+						]
+					},
+					{xtype: 'tbspacer', width: 10}
+						
+				]
+			}			
+		]
+	}, config);
+	
+	FGx.MainViewport.superclass.constructor.call(this, config);
+}, // Constructor	
 
 
-this.initialize = function(){
-	//self.map.setBaseLayer( BASE_LAYERS.osm_light );	
-}
 
-//= Riggered for reshresh now
-this.on_refresh_now = function(){
-	this.store.load();
-}
-
+initialize:  function(){
+	//self.map.setBaseLayer( BASE_LAYERS.osm_light );
+	this.on_open_map("Main Map")
+	
 if(this.refresh_rate > 0){
 	this.runner.start( { run: this.update_flights, interval: this.refresh_rate * 1000 });
 }
-} //< FGx.MainViewport
+},
+
+//= Riggered for reshresh now
+on_refresh_now: function(){
+	this.store.load();
+},
+
+
+}) //< FGx.MainViewport
