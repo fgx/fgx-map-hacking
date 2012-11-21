6d5a0b775b220b54c93e2df3eedebff0d88f0c58
Peter Morgan <pete@daffodil.uk.com>
2012-11-21 02:16:17 +0000

    dodgy

--- fgx-map-ng/fgxmap_pylons/fgx/controllers/ajax_users.py
+++ fgx-map-ng/fgxmap_pylons/fgx/controllers/ajax_users.py
@@ -1,14 +1,15 @@
 
 import logging
 
-from pylons import response
+from pylons import response, request
 from pylons.decorators import jsonify
 from pylons import app_globals
 
 from fgx.lib.base import BaseController, render
 
 from fgx.model import meta
-from fgx.model.mpnet import MpServer
+from fgx.model.secure import User, UserLog
+
 
 log = logging.getLogger(__name__)
 
@@ -17,6 +18,8 @@ class AjaxUsersController(BaseController):
 	@jsonify
 	def users(self):
 		payload = dict(success=True)
+		
+		payload['users'] = [u.dic() for u in meta.Sess.secure.query(User).all()]
 		
 		return payload
 		
